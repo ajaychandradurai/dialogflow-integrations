f7dab9b1033cd5b1a03cafa04a7ba9cbfb24573b
ajaychandradurai <76098543+ajaychandradurai@users.noreply.github.com>
2020-12-16 14:55:54 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
+const projectId = 'Whatsappchatbot';
 const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const accountSid = 'AC509a55bc544036df71781389d2414afb';
+const authToken = '0fe40a3416b79a63c0d7e311b7bb1797';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
